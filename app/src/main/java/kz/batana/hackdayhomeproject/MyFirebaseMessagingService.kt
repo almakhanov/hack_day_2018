de230139f162f26a30c560a86801d8193f1b1902
Askhat Telzhanov <askhat491@gmail.com>
2018-11-09 02:55:29 +0600

    add notification

---
+++ app/src/main/java/kz/batana/hackdayhomeproject/MyFirebaseMessagingService.kt
@@ -0,0 +1,19 @@
+package kz.batana.hackdayhomeproject
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+    private fun sendRegistrationToServer(token:String?){
+
+    }
+}
