88a87295c3742757987ecd9398d7a7e002678516
Askhat Telzhanov <askhat491@gmail.com>
2018-11-09 14:05:04 +0600

    Registration and MainMenu added

---
+++ app/src/main/java/kz/batana/hackdayhomeproject/registration/RegistrationActivity.kt
@@ -0,0 +1,53 @@
+package kz.batana.hackdayhomeproject.registration
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import kotlinx.android.synthetic.main.activity_registration.*
+import kz.batana.hackdayhomeproject.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.title = "Регистрация"
+        actionBar?.apply {
+            this.setDisplayHomeAsUpEnabled(true)
+            this.setDisplayShowHomeEnabled(true)
+            this.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
+            this.setDisplayShowTitleEnabled(true)
+        }
+
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.done ->{
+
+                    true
+                }
+                else->{
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var menuInflater: MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.registration_menu,menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+}
